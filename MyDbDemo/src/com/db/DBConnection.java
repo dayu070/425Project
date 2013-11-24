99522c9231e36a598226caee34bef3c0c2f8cd3d
dayu070 <dayu070@gmail.com>
2013-11-23 21:11:17 -0500

    优化连接
    拆分成静态的连接方法与关闭方法。

--- MyDbDemo/src/com/db/DBConnection.java
+++ MyDbDemo/src/com/db/DBConnection.java
@@ -4,32 +4,63 @@ package com.db;
 import java.sql.*;
 
 public class DBConnection {
-	public static void JDBCExample() throws ClassNotFoundException
+	
+	private static String drivers = DBConfig.DRIVERS;
+	private static String url = DBConfig.URL;
+	private static String user = DBConfig.USER;
+	private static String password = DBConfig.PASSWORD;
+	public static Connection GetConnection() throws ClassNotFoundException
 	{
-	try { 
-			Class.forName ("oracle.jdbc.driver.OracleDriver"); // load driver?
-			Connection conn = DriverManager.getConnection( // connect to server 
-			DBConfig.URL, DBConfig.USER, DBConfig.PASSWORD); 
-			Statement stmt = conn.createStatement(); // create Statement object?
-			PreparedStatement pstmt = null;
+		Connection conn = null;
+		try { 
+				Class.forName (drivers).newInstance();
+		}catch (InstantiationException e)
+		{
+			e.printStackTrace();
+		}catch (IllegalAccessException e)
+		{
+			e.printStackTrace();
+		}catch (ClassNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		
+		try{
+			conn = DriverManager.getConnection(url, user, password);
+		}catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+		
+		return conn;				
+	}
+	
+	public static void Close(Connection conn)
+	{
+		try{
+			if (conn != null && !conn.isClosed())
+				conn.close();
+		}catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String args[]) throws ClassNotFoundException {
+		Connection conn = GetConnection();
+		try
+		{
+			Statement stmt = conn.createStatement(); // create Statement object
 			ResultSet rset = stmt.executeQuery("SELECT ANAME FROM ANIMAL");
 			while (rset.next())
 			{
 				System.out.println(rset.getString("aname"));
 			}
-			stmt.close(); // close Statement and release resources ??
-			conn.close(); // close Connection and release resources
-			
-			}
-			
-			catch (SQLException sqle) { 
-			
-			System.out.println("SQLException : " + sqle);// handle exceptions
-			
-			}
-	}
-	
-	public static void main(String args[]) throws ClassNotFoundException {
-		JDBCExample();
+			stmt.close(); // close Statement and release resources
+			conn.close(); // close Connection and release resources		
+		}
+		catch (SQLException sqle) { 
+			System.out.println("SQLException : " + sqle);// handle exceptions	
+		}
 	}
 }
